a63adb894e82ef1616c6a88f9d512e77e7ea332b
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2016-04-21 12:15:08 -0300

    resolvendo bug

--- Stude/src/Regular.java
+++ Stude/src/Regular.java
@@ -1,26 +1,28 @@
 public class Regular implements MetodoDeEstudo {
-
+	
 	private int intervalo;
-
 	private int tempoDeEstudo;
 	
 	public Regular(){
 		intervalo = 5;
 		tempoDeEstudo = 15;
 	}
+	
+	
 	@Override
-	public int getIntervalo(){
-		return intervalo;
+	public int getTempo() {
+		// TODO Auto-generated method stub
+		return tempoDeEstudo;
 	}
 
 	@Override
-	public int getTempo() {
-		return tempoDeEstudo;
+	public int getIntervalo() {
+		// TODO Auto-generated method stub
+		return intervalo;
 	}
 	
 	@Override
 	public String toString(){
 		return "Metodo de estudo regular";
 	}
-
 }
