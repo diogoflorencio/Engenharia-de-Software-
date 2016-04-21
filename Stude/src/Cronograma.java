46720cf2eeedf0bbf1bf7f0c6323e638c21dea04
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2016-04-20 22:11:05 -0300

    implementando cronograma

--- Stude/src/Cronograma.java
+++ Stude/src/Cronograma.java
@@ -1,24 +1,35 @@
-import java.util.Calendar;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Cronograma {
 
-	private Calendar dataDeInicio;
-
-	private Calendar dataDeTermino;
-
-	private Calendar diasDeEstudo;
-
-	private Calendar horarioDeEstudo;
-
-	private Calendar tempoLivre;
-
-	private Collection<Disciplina> disciplina;
-
-	private Stude stude;
-
-	private Usuario usuario;
-
-	private MetodoDeEstudo metodoDeEstudo;
-
+	private ArrayList<Disciplina> disciplinas;
+	
+	public Cronograma(){
+		disciplinas =  new ArrayList<Disciplina>();
+	}
+	
+	public ArrayList<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+	
+	public Disciplina getDisciplina(String nome){
+		Iterator<Disciplina> it = disciplinas.iterator();
+		Disciplina d ;
+		while(it.hasNext()){
+			d = it.next();
+			if(d.getNome().equals(nome)) return d;
+		}
+		return null;
+	}
+	
+	public void addDisciplina(Disciplina d) throws Exception{
+		if(disciplinas.contains(d)) throw new Exception("Disciplina existente");
+		disciplinas.add(d);
+	}
+	
+	public void removeDisciplina(Disciplina d) throws Exception{
+		if(!disciplinas.contains(d)) throw new Exception("Disciplina inexistente");
+		disciplinas.add(d);
+	}
 }
