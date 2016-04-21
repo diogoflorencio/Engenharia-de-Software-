c5256158b20173d594f986e5ad9b03b052976f87
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2016-04-21 08:28:40 -0300

    adicionando toString

--- Stude/src/Avancado.java
+++ Stude/src/Avancado.java
@@ -9,11 +9,18 @@ public class Avancado implements MetodoDeEstudo {
 		tempoDeEstudo = 30;
 	}
 	
+	@Override
 	public int getTempo() {
 		return tempoDeEstudo;
 	}
-
+	
+	@Override
 	public int getIntervalo() {
 		return intervalo;
+	}
+	
+	@Override
+	public String toString(){
+		return "Metodo de estudo avancado";
 	}
 }
