90d6e9a54dc4a99b19e99b8071b471b3d6ae228b
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2016-04-20 22:29:35 -0300

    implementando desempenho

--- Stude/src/Desempenho.java
+++ Stude/src/Desempenho.java
@@ -1,11 +1,31 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 public class Desempenho {
-
-	private Disciplina disciplinas;
-
-	private Usuario usuario;
-
-	public float calculaCRE() {
-		return 0;
+	
+	public Desempenho(){}
+	
+	public float calculaCRE(List<Disciplina> disciplinas) {
+		if(disciplinas.size() == 0) return 0;
+		float cre = 0;
+		Iterator<Disciplina> it = disciplinas.iterator();
+		Disciplina d;
+		while(it.hasNext()){
+			d = it.next();
+			if(d.getSituacao().equals(Situacao.CONCLUIDA)) cre += d.calculaMedia()/disciplinas.size();
+		}
+		return cre;
 	}
-
+	
+	public HashMap<Disciplina, Double> tempoDeEstudo(List<Disciplina> disciplinas){
+		HashMap<Disciplina, Double> desempenho = new HashMap<Disciplina, Double>();
+		Iterator<Disciplina> it =  disciplinas.iterator();
+		Disciplina d;
+		while(it.hasNext()){
+			d = it.next();
+			desempenho.put(d, d.getHorasEstudadas());
+		}
+		return desempenho;
+	}
 }
