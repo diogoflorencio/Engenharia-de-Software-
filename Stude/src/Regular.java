50410d7e017358b6450266cc9e6aaa5315ed68d9
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2016-04-20 22:31:52 -0300

    implementando metodo de estudo regular

--- Stude/src/Regular.java
+++ Stude/src/Regular.java
@@ -2,14 +2,19 @@ public class Regular implements MetodoDeEstudo {
 
 	private int intervalo;
 
-	private int tempo;
-
-	public void setTempo(int tempo) {
-
+	private int tempoDeEstudo;
+	
+	public Regular(){
+		intervalo = 5;
+		tempoDeEstudo = 15;
 	}
 
-	public void setIntervalo(int tempo) {
-
+	public int getIntervalo(){
+		return intervalo;
+	}
+	
+	public int getTempoDeEstudo(){
+		return tempoDeEstudo;
 	}
 
 }
